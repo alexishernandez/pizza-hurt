61f495a6ba94dd66f97d57b8b9630b7a5d83c765
Alexis Hernandez <>
2023-09-16 08:42:09 -0300

    Se agregan mas entidades

--- src/main/java/uy/com/curso/pizzahurt/entities/Pedido.java
+++ src/main/java/uy/com/curso/pizzahurt/entities/Pedido.java
@@ -1,31 +1,38 @@
 package uy.com.curso.pizzahurt.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uy.com.curso.pizzahurt.entities.common.AbstractEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario extends AbstractEntity {
+public class Pedido extends AbstractEntity {
 
-    private String nombreCompleto;
-    private String email;
-    private String telefono;
-    private String password;
-    private Boolean activo;
+    //Usuario que realiza pedido
+    @ManyToOne
+    private Usuario usuario;
 
-    //domicilio del usuario
+    //Lugar de entrega
+    private String nombreReceptor;
     private String ciudad;
     private String barrio;
     private String direccion;
 
-    //Tarjeta del usuario
+    //Medio de Pago
     private String nroTarjeta;
     private String fechaVencimiento;
     private String codigoCVV;
 
+    //ordenes
+    @OneToMany
+    private List<Pizza> pizzas = new ArrayList<>();
 }
