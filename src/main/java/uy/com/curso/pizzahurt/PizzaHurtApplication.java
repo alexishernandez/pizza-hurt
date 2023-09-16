3b1e8287038ac1b3bdbe522faff3b305d4837bcb
Alexis Hernandez <>
2023-09-16 03:25:46 -0300

    Initial commit

---
+++ src/main/java/uy/com/curso/pizzahurt/PizzaHurtApplication.java
@@ -0,0 +1,13 @@
+package uy.com.curso.pizzahurt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaHurtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaHurtApplication.class, args);
+	}
+
+}
