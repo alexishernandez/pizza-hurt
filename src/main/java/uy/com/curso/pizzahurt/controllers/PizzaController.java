8dbee074469b1a03261477b769830fef508de7c5
Jorge Gastaldi <jgastald@gmail.com>
2023-09-20 02:02:29 -0300

    Controller

---
+++ src/main/java/uy/com/curso/pizzahurt/controllers/PizzaController.java
@@ -0,0 +1,15 @@
+package uy.com.curso.pizzahurt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PizzaController {
+	
+	 @GetMapping("/") 
+	 public String index() {
+		 return "index";
+	 }
+	 
+
+}
