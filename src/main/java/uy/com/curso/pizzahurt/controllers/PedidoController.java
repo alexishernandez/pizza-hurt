87f0fc079437373d0708688a9f06337b6b6e804c
Jorge Gastaldi <jgastald@gmail.com>
2023-09-24 22:17:57 -0300

    - Fragment campoDTO para agregar campos facilmente y un algo del pedido

---
+++ src/main/java/uy/com/curso/pizzahurt/controllers/PedidoController.java
@@ -0,0 +1,41 @@
+package uy.com.curso.pizzahurt.controllers;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import uy.com.curso.pizzahurt.models.Pedido;
+import uy.com.curso.pizzahurt.services.PedidoService;
+
+@RequestMapping("/pedido")
+@Controller
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+    
+    @GetMapping("/crear")
+    public String crearPedido() {
+    	return ("editPedido");
+    }
+
+    @GetMapping("/show")
+    public String showPedido(Model model, @RequestParam(name="id") Long id) {
+        Optional<Pedido> pedido = pedidoService.find(id);
+        if (pedido.isPresent()){
+            model.addAttribute("pedido",pedido.get());
+            return "editPedido";
+        }else{
+            return "editPedido";
+            }
+    }
+
+
+}
--- src/main/java/uy/com/curso/pizzahurt/repositories/PedidoRepository.java
+++ src/main/java/uy/com/curso/pizzahurt/repositories/PedidoRepository.java
@@ -3,6 +3,6 @@ package uy.com.curso.pizzahurt.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uy.com.curso.pizzahurt.models.Pedido;
 
-interface PedidoRepository extends JpaRepository<Pedido, Long> {
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
 }
